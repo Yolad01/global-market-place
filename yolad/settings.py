beee9adacc720ffac6eef8cfc7e6540a79d45f81
Chiemerie Okoro <chiemeriedroid@gmail.com>
2024-04-05 16:18:20 +0100

    fixed migration issues-1

--- yolad/settings.py
+++ yolad/settings.py
@@ -31,7 +31,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-2l3o0(u8z&xz1age*felsx@chp2@z!3u4otvuh#_2_i@f224+w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
@@ -189,5 +188,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # remember to r
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'chiemeriedroid@gmail.com'
-EMAIL_HOST_PASSWORD = 'N@n$er1234'
+# EMAIL_HOST_PASSWORD = 'N@n$er1234'
 EMAIL_USE_TLS = True
