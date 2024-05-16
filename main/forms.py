f0dfed0acc22078087db0ab30b7b62ae1b165e56
Chiemerie Okoro <chiemeriedroid@gmail.com>
2024-05-16 12:06:00 +0100

    updated messaging UI

--- main/forms.py
+++ main/forms.py
@@ -240,7 +240,7 @@ class BriefAppForm(forms.Form):
 
 class ChatMessageForm(forms.Form):
     msg_body = forms.CharField(widget=forms.Textarea(attrs={
-        'rows': 2, 'cols': 48, "class": "border border-veryDarkGreen p-2 mx-3 rounded-md resize-none",
+        'rows': 5, 'cols': 48, "class": "border border-veryDarkGreen p-2 mx-3 rounded-md resize-none",
         "placeholder": "Enter your messsage here"
     }))
 
