3a3266cd8bd150a7f54d72882c2ada8e111d3d8f
Chiemerie Okoro <chiemeriedroid@gmail.com>
2024-06-05 22:06:08 +0100

    update

--- main/payment.py
+++ main/payment.py
@@ -26,16 +26,18 @@ class PayStackIt:
         self.SECRET_KEY = api_key
         self.callback_url = callback_url
         self.on_cancel_url = on_cancel_url
+        self.headers = {
+            "Authorization": f'Bearer {self.SECRET_KEY}',
+            "Content-Type": "application/json"
+        }
+
 
     def pay(self, amount: int, email: str):
         """Returns a dictionary object."""
         self.email = email
         self.amount = amount * 100
         url_initialize = "https://api.paystack.co/transaction/initialize"
-        self.headers = {
-            "Authorization": f'Bearer {self.SECRET_KEY}',
-            "Content-Type": "application/json"
-        }
+        self.headers = self.headers
         self.data = {
             "email": self.email,
             "amount": self.amount,
@@ -60,12 +62,10 @@ class PayStackIt:
 
         return None
     
+
     def verify_transaction(self, reference_code) -> dict:
         self.transaction_verification_url = f'https://api.paystack.co/transaction/verify/{reference_code}'
-        self.headers = {
-            "Authorization": f'Bearer {self.SECRET_KEY}',
-            "Content-Type": "application/json"
-        }
+        self.headers = self.headers
         self.response = requests.get(
             url=self.transaction_verification_url,
             headers=self.headers,
@@ -78,8 +78,25 @@ class PayStackIt:
         self.card_type = self.response["data"]["authorization"]["card_type"]
         self.payment_channel = self.response["data"]["channel"]
 
-        return self.response
+        return None
+    
+    
 
+    def refund(self, reference_code: str):
+        self.refund_url ="https://api.paystack.co/refund"
+        self.reference_code = reference_code
+        self.data = {
+            "transaction": self.reference_code,
+        }
+        self.response = requests.post(
+            url=self.refund_url,
+            headers=self.headers,
+            json=self.data
+        )
+        if self.response.status_code == 200:
+            return self.response.json()
+        else:
+            return None
 
     
 
