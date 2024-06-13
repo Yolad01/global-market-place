21c4b3f392239454fb4703ceb5aedc574468e346
Isinwa-Deborah <debbyisinwa@gmail.com>
2024-06-13 13:35:59 -0700

    Merge resolutions

--- yolad/settings.py
+++ yolad/settings.py
@@ -89,17 +89,17 @@ TEMPLATES = [
 ASGI_APPLICATION = 'yolad.routing.application'
 
 # CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [("127.0.0.1", 6379)],
-#         },
-#     },
+# "default": {
+# "BACKEND": "channels_redis.core.RedisChannelLayer",
+# "CONFIG": {
+# "hosts": [("127.0.0.1", 6379)],
+# },
+# },
 # }
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",  # Use InMemoryChannelLayer for testing/dev
+        "BACKEND": "channels.layers.InMemoryChannelLayer", # Use InMemoryChannelLayer for testing/dev
         # For production use, consider using Redis or other suitable backends.
     },
 }
@@ -120,7 +120,7 @@ DATABASES = {
 
 
 # DATABASES = {
-#     'default':  Env.db_url_config(env('DATABASE_URL'))
+# 'default': Env.db_url_config(env('DATABASE_URL'))
 # }
 
 
