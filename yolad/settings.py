936a1909bfb059598a66dcc09ee8e8cb69e59e85
Chiemerie Okoro <chiemeriedroid@gmail.com>
2024-06-25 11:27:18 +0100

    chat update

--- yolad/settings.py
+++ yolad/settings.py
@@ -37,10 +37,10 @@ DEBUG = env("DEBUG")
 
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = ["getskillas.com", "www.getskillas.com", "localhost", "185.158.132.69"]
+ALLOWED_HOSTS = ["getskillas.com", "www.getskillas.com", "localhost", "185.158.132.69", "https://getskillas.com", "https://www.getskillas.com"]
 
 
-CSRF_TRUSTED_ORIGINS=["https://getskillas.com", "https://www.getskillas.com", "getskillas.com", "www.getskillas.com"]
+CSRF_TRUSTED_ORIGINS=["https://getskillas.com", "https://www.getskillas.com"]
 
 
 
