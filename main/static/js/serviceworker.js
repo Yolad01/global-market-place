a7f132d121f2619266bca837e7b7af2b85fe745a
Chiemerie Okoro <chiemeriedroid@gmail.com>
2024-06-28 16:56:25 +0100

    update on PWA

--- main/static/js/serviceworker.js
+++ main/static/js/serviceworker.js
@@ -77,3 +77,12 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    console.log('Service Worker registered with scope:', registration.scope);
+  }).catch(function(error) {
+    console.log('Service Worker registration failed:', error);
+  });
+}
